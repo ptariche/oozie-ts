9eebdf4d84c23f30f98eee7682a22ba57d9a061b
Peter A. Tariche <ptariche@gmail.com>
2019-06-04 01:40:58 -0700

    need to test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import {
   apiVersion,
   serviceVersion,
   httpMethods,
-  typeOozieConfig, 
+  typeOozieConfig,
+  versionFunction,
   retrieveFunction,
   statusFunction,
   setStatusFunction,
@@ -13,7 +14,15 @@ import {
   instrumentationFunction,
   buildVersionFunction,
   availableTimezonesFunction,
-  queueDumpFunction
+  queueDumpFunction,
+  jobSubmissionFunction,
+  jobSubmissionMapReduceFunction,
+  jobSubmissionPigFunction,
+  jobSubmissionHiveFunction,
+  manageJobFunction,
+  jobAction,
+  getJobFunction,
+  getJobsFunction
 } from './types/';
 
 class Oozie {
@@ -47,16 +56,16 @@ class Oozie {
     this.config = config;
   };
 
-  private retrieve:retrieveFunction = (method, route, body) => {
+  private retrieve:retrieveFunction = (method, route, body, options) => {
     switch (method) {
       case httpMethods.GET: 
         return this.axios[method](route);
       break;
       case httpMethods.PUT: 
-        return this.axios[method](route, body);
+        return this.axios[method](route, body, options);
       break;
       case httpMethods.POST: 
-        return this.axios[method](route, body);
+        return this.axios[method](route, body, options);
       break;
       case httpMethods.DELETE: 
         return this.axios[method](route);
@@ -64,51 +73,242 @@ class Oozie {
     }
   };
 
+  private xmlHeader = () => {
+    return {headers: {'Content-Type': 'text/xml'}};
+  }
+
+  private xmlStatement = () => {
+    return `<?xml version="1.0" encoding="UTF-8"?>`;
+  }
+
   private getVersion = (version: number) => {
     return version ? version : this.config.apiVersion;
   }
 
-  public versions = async () => {
-    return await this.retrieve(httpMethods.GET, '/versions', null);
+  public versions:versionFunction = async () => {
+    return await this.retrieve(httpMethods.GET, '/versions');
   };
 
   public status:statusFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/status`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/status`);
   };
 
-  public setStatus:setStatusFunction  = async(mode, version) => {
+  public setStatus:setStatusFunction  = async (mode, version) => {
+    if (!mode) throw new Error('Mode is required');
     return await this.retrieve(httpMethods.PUT, `/v${this.getVersion(version)}/admin/status?systemmode=${mode}`, null);
   };
 
   public environment:environmentFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/os-env`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/os-env`);
   };
 
   public javaSystem:javaSystemFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/java-sys-properties`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/java-sys-properties`);
   };
 
   public configuration:configurationFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/configuration`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/configuration`);
   };
 
   public instrumentation:instrumentationFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/instrumentation`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/instrumentation`);
   };
 
   public buildVersion:buildVersionFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/build-version`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/build-version`);
   };
 
   public availableTimezones:availableTimezonesFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/available-timezones`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/available-timezones`);
   };
 
   public queueDump:queueDumpFunction = async (version) => {
-    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/queue-dump`, null);
+    return await this.retrieve(httpMethods.GET, `/v${this.getVersion(version)}/admin/queue-dump`);
   };
 
-}
+  public createJob:jobSubmissionFunction = async (xml, version) => {
+    if (!xml) throw new Error('XML is required');
+    return await this.retrieve(httpMethods.POST, `/v${this.getVersion(version)}/jobs`, xml, this.xmlHeader());
+  };
+
+  public createProxyMapReduceJob:jobSubmissionMapReduceFunction = async (options) => {
+    if (!options) throw new Error('options are required');
+    if (!options.nameNode) throw new Error('nameNode is required');
+    if (!options.jobTracker) throw new Error('jobTracker is required');
+    if (!options.mapTaskClassname) throw new Error('mapTaskClassname is required');
+    if (!options.reducerClassname) throw new Error('reducerClassname is required');
+    if (!options.inputDirectory) throw new Error('inputDirectory is required');
+    if (!options.outputDirectory) throw new Error('outputDirectory is required');
+    if (!options.oozieLibPath) throw new Error('oozieLibPath is required');
+    if (!options.userName) throw new Error('userName is required');
+
+    const xml = `
+      ${this.xmlStatement()}
+      <configuration>
+          <property>
+              <name>fs.default.name</name>
+              <value>${options.nameNode}</value>
+          </property>
+          <property>
+              <name>mapred.job.tracker</name>
+              <value>${options.jobTracker}</value>
+          </property>
+          <property>
+              <name>mapred.mapper.class</name>
+              <value>${options.mapTaskClassname}</value>
+          </property>
+          <property>
+              <name>mapred.reducer.class</name>
+              <value>${options.reducerClassname}</value>
+          </property>
+          <property>
+              <name>mapred.input.dir</name>
+              <value>${options.inputDirectory}</value>
+          </property>
+          <property>
+              <name>mapred.output.dir</name>
+              <value>${options.outputDirectory}</value>
+          </property>
+          <property>
+              <name>user.name</name>
+              <value>${options.userName}</value>
+          </property>
+          <property>
+              <name>oozie.libpath</name>
+              <value>${options.oozieLibPath}</value>
+          </property>
+          <property>
+              <name>oozie.proxysubmission</name>
+              <value>${options.oozieProxySubmission || 'true'}</value>
+          </property>
+      </configuration>
+    `;
+    return await this.retrieve(httpMethods.POST, `/v${this.getVersion(options.apiVersion)}/jobs?type=mapreduce`, xml, this.xmlHeader());
+  };
+  
+  public createProxyPigJob:jobSubmissionPigFunction = async (options) => {
+    if (!options) throw new Error('options are required');
+    if (!options.nameNode) throw new Error('nameNode is required');
+    if (!options.jobTracker) throw new Error('jobTracker is required');
+    if (!options.pigScript) throw new Error('pigScript is required');
+    if (!options.oozieLibPath) throw new Error('oozieLibPath is required');
+    if (!options.userName) throw new Error('userName is required');
+
+    const xml = `
+      ${this.xmlStatement()}
+      <configuration>
+          <property>
+              <name>fs.default.name</name>
+              <value>${options.nameNode}</value>
+          </property>
+          <property>
+              <name>mapred.job.tracker</name>
+              <value>${options.jobTracker}</value>
+          </property>
+          <property>
+              <name>oozie.pig.script</name>
+              <value>${options.pigScript}</value>
+          </property>
+          <property>
+              <name>oozie.libpath</name>
+              <value>${options.oozieLibPath}</value>
+          </property>
+          <property>
+              <name>user.name</name>
+              <value>${options.userName}</value>
+          </property>
+          <property>
+              <name>oozie.proxysubmission</name>
+              <value>${options.oozieProxySubmission || 'true'}</value>
+          </property>
+      </configuration>
+    `;
+    return await this.retrieve(httpMethods.POST, `/v${this.getVersion(options.apiVersion)}/jobs?type=pig`, xml, this.xmlHeader());
+  };
+
+  public createHiveJob:jobSubmissionHiveFunction = async (options) => {
+    if (!options) throw new Error('options are required');
+    if (!options.nameNode) throw new Error('nameNode is required');
+    if (!options.jobTracker) throw new Error('jobTracker is required');
+    if (!options.hiveScript) throw new Error('hiveScript is required');
+    if (!options.oozieLibPath) throw new Error('oozieLibPath is required');
+    if (!options.userName) throw new Error('userName is required');
+
+    const xml = `
+      ${this.xmlStatement()}
+      <configuration>
+          <property>
+              <name>fs.default.name</name>
+              <value>${options.nameNode}</value>
+          </property>
+          <property>
+              <name>mapred.job.tracker</name>
+              <value>${options.jobTracker}</value>
+          </property>
+          <property>
+            <name>oozie.hive.script</name>
+            <value>
+              ${options.hiveScript}
+            </value>
+          </property>
+          <property>
+              <name>user.name</name>
+              <value>${options.userName}</value>
+          </property>
+          <property>
+              <name>oozie.libpath</name>
+              <value>${options.oozieLibPath}</value>
+          </property>
+          <property>
+              <name>oozie.proxysubmission</name>
+              <value>${options.oozieProxySubmission || 'true'}</value>
+          </property>
+      </configuration>
+    `;
+    return await this.retrieve(httpMethods.POST, `/v${this.getVersion(options.apiVersion)}/jobs?type=mapreduce`, xml, this.xmlHeader());
+  };
+
+  public manageJob:manageJobFunction = async (jobId, action, xml, options, version) => {
+    if (!jobId) throw new Error('An jobId is required');
+    if (!action) throw new Error('An action is required');
+
+    let path: string = `/v${this.getVersion(version)}/job/${jobId}?action=${action}`;
+
+    if (action === jobAction.COORD_RERUN) {
+      path+= `&type=${options.type}&refresh=${options.refresh ? options.refresh : false}&nocleanup=${options.nocleanup ? options.nocleanup : false}${options.scope ? `&scope=${options.scope}` : ''}`;
+    } else if ( action === jobAction.BUNDLE_RERUN) {
+      path+= `&refresh=${options.refresh ? options.refresh : false}&nocleanup=${options.nocleanup ? options.nocleanup : false}`;
+    } else if ( action === jobAction.CHANGE) {
+      if (!options.value) throw new Error('A Value is required to be changed');
+      path+= `&value=${options.value}`;
+    }
+
+    return await this.retrieve(httpMethods.PUT, path, xml ? xml : null);
+  };
+
+  public getJob:getJobFunction = async (jobId, show, timezone, version) => {
+    if (!jobId) throw new Error('An jobId is required');
+
+    let path:string = `/v${this.getVersion(version)}/job/${jobId}?`;
+
+    if (show)     path += `show=${show}`;
+    if (timezone) path += `&timezone=${timezone}`;
+
+    return await this.retrieve(httpMethods.GET, path);
+  };
+
+  public getJobs:getJobsFunction = async (filter, offset, limit, timezone, version) => {
+    let path:string = `/v${this.getVersion(version)}/jobs?`;
+
+    if (filter)   path += `filter=${encodeURIComponent(filter)}`;
+    if (offset)   path += `&offset=${offset}`;
+    if (limit)    path += `&len=${limit}`;
+    if (timezone) path += `&timezone=${timezone}`;
+
+    return await this.retrieve(httpMethods.GET, path);
+  };
+
+};
 
 export default Oozie;
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -23,78 +23,214 @@ type typeOozieConfig = {
  * @param method - HTTP Method: get,post,put,delete,patch
  * 
 */
-type retrieveFunction = (method: httpMethods, route: string, body: object|null) =>  Promise<object|null>;
+type retrieveFunction = (method: httpMethods, route: string, body?: object|string|null|undefined, options?: object|null|undefined) =>  Promise<any>;
 
 /**
  * @remarks
  * Lookup the Status of Oozie
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Override the API version from your config
 */
-type statusFunction = (apiVersion: number) =>  Promise<object|null>;
+type versionFunction = () =>  Promise<number[]>;
+
+/**
+ * @remarks
+ * Lookup the Status of Oozie
+ *
+ * @param apiVersion - Optional* Override the API version from your config
+*/
+type statusFunction = (apiVersion?: apiVersion) =>  Promise<{systemMode: systemMode}|object|null>;
 
 /**
  * @remarks
  * Set the Status of Oozie
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type setStatusFunction = (mode: systemMode, apiVersion: number) =>  Promise<object|null>;
+type setStatusFunction = (mode: systemMode, apiVersion: apiVersion) =>  Promise<null|undefined>;
 
 /**
  * @remarks
  * Lookup the OS Environment
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type environmentFunction = (apiVersion: number) =>  Promise<object|null>;
+type environmentFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
 
 /**
  * @remarks
  * Lookup the Java System Properties
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type javaSystemFunction = (apiVersion: number) =>  Promise<object|null>;
+type javaSystemFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
 
 /**
  * @remarks
  * Lookup the Oozie Configuration
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type configurationFunction = (apiVersion: number) =>  Promise<object|null>;
+type configurationFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
 
 /**
  * @remarks
  * Lookup the Oozie Instrumentation Information
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type instrumentationFunction = (apiVersion: number) =>  Promise<object|null>;
+type instrumentationFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
 
 /**
  * @remarks
  * Lookup the Oozie Build Version Information
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type buildVersionFunction = (apiVersion: number) =>  Promise<object|null>;
+type buildVersionFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
 
 /**
  * @remarks
  * Lookup the Oozie Available Timezones
  *
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type availableTimezonesFunction = (apiVersion: number) =>  Promise<object|null>;
+type availableTimezonesFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
 
 /**
  * @remarks
  * Helps Debug by Dumping the Queue
- * @param apiVersion - Ovveride the API version from your config
+ * @param apiVersion - Optional* Override the API version from your config
 */
-type queueDumpFunction = (apiVersion: number) =>  Promise<object|null>;
+type queueDumpFunction = (apiVersion?: apiVersion) =>  Promise<object|null>;
+
+/**
+ * @remarks
+ * Submit a job with XML Instructions
+ * @param xml - XML to Post for Job Creation
+ * @param apiVersion - Optional* Override the API version from your config
+*/
+type jobSubmissionFunction = (xml: string, apiVersion?: apiVersion) =>  Promise<jobType>;
+
+/**
+ * @remarks
+ * You can submit a Workflow that contains a single MapReduce action without writing a workflow.xml. Any required Jars or other files must already exist in HDFS.
+ * @param options - XML to Post for Job Creation
+ * @param options.nameNode - hdfs://localhost:8020
+ * @param options.jobTracker - localhost:8021
+ * @param options.mapTaskClassname - org.apache.oozie.example.SampleMapper
+ * @param options.reducerClassname - org.apache.oozie.example.SampleReducer
+ * @param options.inputDirectory - hdfs://localhost:8020/user/rkanter/examples/input-data/text
+ * @param options.outputDirectory - hdfs://localhost:8020/user/rkanter/examples/output-data/map-reduce
+ * @param options.userName - fakeUser
+ * @param options.oozieLibPath - hdfs://localhost:8020/user/rkanter/examples/apps/map-reduce/lib
+ * @param options.oozieProxySubmission - true
+ * @param options.apiVersion - Optional* Override the API version from your config
+*/
+type jobSubmissionMapReduceFunction = (options: jobMapReduceOptions) =>  Promise<jobType>;
+
+/**
+ * @remarks
+ * You can submit a Workflow that contains a single Pig action without writing a workflow.xml. Any requred Jars or other files must already exist in HDFS.
+ * @param options - XML to Post for Job Creation
+ * @param options.nameNode - hdfs://localhost:8020
+ * @param options.jobTracker - localhost:8021
+ * @param options.pigScript -  Contains the pig script you want to run (the actual script, not a file path)
+ * @param options.oozieLibPath - A directory in HDFS that contains necessary Jars for your job
+ * @param options.userName - fakeUser
+ * @param options.oozieProxySubmission - true
+ * @param options.apiVersion - Optional* Override the API version from your config
+*/
+type jobSubmissionPigFunction = (options: jobPigOptions) => Promise<jobType>;
+
+/**
+ * @remarks
+ * You can submit a Workflow that contains a single Hive action without writing a workflow.xml. Any requred Jars or other files must already exist in HDFS.
+ * @param options - XML to Post for Job Creation
+ * @param options.nameNode - hdfs://localhost:8020
+ * @param options.jobTracker - localhost:8021
+ * @param options.hiveScript -  Contains the hive script you want to run (the actual script, not a file path)
+ * @param options.oozieLibPath - A directory in HDFS that contains necessary Jars for your job
+ * @param options.userName - fakeUser
+ * @param options.oozieProxySubmission - true
+ * @param options.apiVersion - Optional* Override the API version from your config
+*/
+type jobSubmissionHiveFunction = (options: jobHiveOptions) => Promise<jobType>;
+
+/**
+ * @remarks
+ * You can submit a Workflow that contains a single Hive action without writing a workflow.xml. Any requred Jars or other files must already exist in HDFS.
+ * @param jobId - The JobID
+ * @param action -  start, rerun, suspend, resume, dryrun, & change
+ * @param xml - *Required XML if required for re-running or changing the job for example
+ * @param options - Options for rerun
+ * @param options.type - date or action
+ * @param options.refresh - boolean
+ * @param options.nocleanup - boolean
+ * @param options.scope - A comma-separated list of date ranges. Each date range element is specified with dates separated by :: * action : a comma-separated list of action ranges. Each action range is specified with two action numbers separated by -
+ * @param options.value - The value parameter can contain any of the following: * endtime: the end time of the coordinator job. * concurrency: the concurrency of the coordinator job. * pausetime: the pause time of the coordinator job.
+*/
+type manageJobFunction = (jobId: string, action: jobAction, xml?: string, options?: {type?: jobActionRerunType, refresh?: boolean, nocleanup?: boolean, scope?: string, value?: string}, apiVersion?: apiVersion) => Promise<null|undefined>;
+
+/**
+ * @remarks
+ * Get a Job
+ * @param jobId - The JobID
+ * @param show -  Optional* definition, log, & info
+ * @param timezone -  Optional* timezone GMT, PST, etc
+ * @param apiVersion - Optional* Override the API version from your config
+*/
+type getJobFunction = (jobId: string, show?: jobGetType, timezone?: timezone, apiVersion?: apiVersion) =>  Promise<any>;
+
+/**
+ * @remarks
+ * Get Jobs
+ * @param filter - [NAME=VALUE][;NAME=VALUE]*
+ * @param offset - The numerical value to offset by
+ * @param limit - The numerical value to limit by
+ * @param timezone -  Optional* timezone GMT, PST, etc
+ * @param apiVersion - Optional* Override the API version from your config
+*/
+type getJobsFunction = (filter?: string, offset?: number, limit?: number, timezone?: timezone, apiVersion?: apiVersion)  => Promise<object|null>;
+
+type jobMapReduceOptions = {
+  nameNode: string, 
+  jobTracker: string,
+  mapTaskClassname: string,
+  reducerClassname: string,
+  inputDirectory: string,
+  outputDirectory: string,
+  userName: string,
+  oozieLibPath: string,
+  oozieProxySubmission?: boolean|null|undefined,
+  apiVersion?: apiVersion
+};
+
+type jobPigOptions = {
+  nameNode: string, 
+  jobTracker: string,
+  pigScript: string,
+  oozieLibPath: string,
+  userName: string,
+  oozieProxySubmission?: boolean|null|undefined
+  apiVersion?: apiVersion
+};
+
+type jobHiveOptions = {
+  nameNode: string, 
+  jobTracker: string,
+  hiveScript: string,
+  oozieLibPath: string,
+  userName: string,
+  oozieProxySubmission?: boolean|null|undefined
+  apiVersion?: apiVersion
+};
+
+/**
+ * @remarks
+ * Job Response
+*/
+type jobType = {id: string}|object|null;
 
 enum serviceVersion {
   FOUR = 4,
@@ -126,7 +262,56 @@ enum jobAction {
   RESUME = 'resume',
   DRYRUN = 'dryrun',
   RERUN = 'rerun',
-  CHANGE = ' change'
+  COORD_RERUN = 'coord-rerun',
+  BUNDLE_RERUN = 'bundle-rerun',
+  CHANGE = 'change'
+};
+
+enum jobActionRerunType {
+  DATE = 'date',
+  ACTION = 'action'
+};
+
+enum jobGetType {
+  LOG = 'log',
+  GRAPTH = 'graph',
+  DEFINITION = 'definition',
+  INFO = 'info'
+}
+
+enum timezone {
+  GMT = 'GMT',
+  UTC = 'GMT',
+  ECT = 'GMT+1:00',
+  EET = 'GMT+2:00',
+  ART = 'GMT+2:00',
+  EAT = 'GMT+3:00',
+  MET = 'GMT+3:30',
+  NET = 'GMT+4:00',
+  PLT = 'GMT+5:00',
+  IST = 'GMT+5:30',
+  BST = 'GMT+6:00',
+  VST = 'GMT+7:00',
+  CTT = 'GMT+8:00',
+  JST = 'GMT+9:00',
+  ACT = 'GMT+9:30',
+  AET = 'GMT+10:00',
+  SST = 'GMT+11:00',
+  NST = 'GMT+12:00',
+  MIT = 'GMT-11:00',
+  HST = 'GMT-10:00',
+  AST = 'GMT-9:00',
+  PST = 'GMT-8:00',
+  PNT = 'GMT-7:00',
+  MST = 'GMT-7:00',
+  CST = 'GMT-6:00',
+  EST = 'GMT-5:00',
+  IET = 'GMT-5:00',
+  PRT = 'GMT-4:00',
+  CNT = 'GMT-3:30',
+  AGT = 'GMT-3:00',
+  VET = 'GMT-3:00',
+  CAT = 'GMT-1:00'
 };
 
 export {
@@ -137,6 +322,7 @@ export {
   jobAction,
   typeOozieConfig,
   retrieveFunction,
+  versionFunction,
   statusFunction,
   setStatusFunction,
   environmentFunction,
@@ -145,5 +331,14 @@ export {
   instrumentationFunction,
   buildVersionFunction,
   availableTimezonesFunction,
-  queueDumpFunction
+  queueDumpFunction,
+  jobSubmissionFunction,
+  jobSubmissionMapReduceFunction,
+  jobSubmissionPigFunction,
+  jobSubmissionHiveFunction,
+  manageJobFunction,
+  jobGetType,
+  timezone,
+  getJobFunction,
+  getJobsFunction
 };
